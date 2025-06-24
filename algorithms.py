db4e547c264d8ea73b26bbac37829ab2ab3bacd4
SUDHARSAN_T <sudharsant44@gmail.com>
2025-06-24 22:53:54 +0530

    2nd

--- algorithms.py
+++ algorithms.py
@@ -5,6 +5,12 @@ def count_even(numbers):
             result += 1
     return result
 
-print(count_even([1, 2, 3])) 
-print(count_even([2, 2, 2, 2, 2])) 
+def count_even2(numbers):
+    return sum(1 for x in numbers if x % 2 == 0)
+
+print(count_even([1, 9, 4]))         
+print(count_even([9, 2, 5, 12, 72]))  
 print(count_even([5, 4, 1, 7, 9, 6])) 
+print(count_even2([1, 2, 3]))         
+print(count_even2([2, 2, 2, 2, 2]))   
+print(count_even2([5, 4, 1, 7, 9, 6]))
