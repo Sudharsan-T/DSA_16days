6269fe684dea09008d6418ff229154a31452204b
SUDHARSAN_T <sudharsant44@gmail.com>
2025-06-25 09:29:02 +0530

    Algorithm Basic

--- algorithms.py
+++ algorithms.py
@@ -1,16 +1,10 @@
-def count_even(numbers):
-    result = 0
-    for x in numbers:
-        if x % 2 == 0:
-            result += 1
-    return result
+def count_even(numbers, version = 1):
+    if version == 1:
+        result = 0
+        for x in numbers:
+            if x % 2 == 0:
+                result += 1
+        return result
+    elif version == 2:
+        return sum(1 for x in numbers if x % 2 == 0)
 
-def count_even2(numbers):
-    return sum(1 for x in numbers if x % 2 == 0)
-
-print(count_even([1, 9, 4]))         
-print(count_even([9, 2, 5, 12, 72]))  
-print(count_even([5, 4, 1, 7, 9, 6])) 
-print(count_even2([1, 2, 3]))         
-print(count_even2([2, 2, 2, 2, 2]))   
-print(count_even2([5, 4, 1, 7, 9, 6]))
