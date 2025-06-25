edcecb9fde43e3327fdd02a939ff0b1a16f38928
SUDHARSAN_T <sudharsant44@gmail.com>
2025-06-25 09:32:59 +0530

    Max_diff

--- algorithms.py
+++ algorithms.py
@@ -8,3 +8,16 @@ def count_even(numbers, version = 1):
     elif version == 2:
         return sum(1 for x in numbers if x % 2 == 0)
 
+#max_diff
+def max_diff(numbers, version = 1):
+    if version == 1:
+        result = 0
+        for x in numbers:
+            for y in numbers:
+                result = mmax(result, abs(x - y))
+        return result
+    elif version == 2:
+        numbers = sorted(numbers)
+        return numbers[-1] - numbers[0]
+    elif version == 3:
+        return max(numbers) - min(numbers) #efficient way
