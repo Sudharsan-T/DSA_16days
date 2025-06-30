69a58ce2dd9898aee94c974a66135b781c5d345d
SUDHARSAN_T <sudharsant44@gmail.com>
2025-06-30 22:26:13 +0530

    Algo Final

--- algorithms.py
+++ algorithms.py
@@ -28,3 +28,46 @@ def count_even(numbers):
         if x % 2 == 0:
             result += 1
     return result
+
+#Time complexity
+#Constant Time - no loops[O(1)]:
+def middle(numbers):
+    n =len(numbers)
+    return numbers[n // 2]
+
+#Single loop [O(n)]:
+def calc_sum(numbers):
+    result = 0
+    for x in numbers:
+        result += x
+    return result
+
+#Nested Loops [O(n^2)]
+def has_sum(number, x):
+    for a in numbers:
+        for b in numbers:
+            if a + b == x:
+                return True
+    return False
+#If algorithm has 'K' nested loops each of which goes through all elements of the input [O(n^k)]
+
+#Sequential Code Segments
+def count_min(numbers):
+    #stage-1
+    min_value = numbers[0]
+    for x in numbers:
+        if x < min_value:
+            min_value = x
+    #stage-2
+    result = 0
+    for x in numbers:
+        if x == min_value:
+            result += 1
+
+    return result
+
+'''
+Stage 1's TC is O(n)
+Stage 2's TC is also O(n)
+So combined TC will also be O(n)
+'''
